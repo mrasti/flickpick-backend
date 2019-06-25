e06974c24a46e0841b31ed2bc245d19d6b4deb56
Melika <melika.r@gmail.com>
2019-06-25 01:40:03 -0400

    convert indentation to space in seed-movies.js

--- db/seed-movies.js
+++ db/seed-movies.js
@@ -9,46 +9,46 @@ let maxCalls = 100
 Movie.deleteMany({}).then(() => {
     console.log("All movies deleted!")
 }).then(async () => {
-	for(var p=1; p<=maxCalls; p++){
-		var waitTime = 0;
-		if(p % 40=== 0) {
-			waitTime = 10000;
-		}
-		
-		await new Promise(resolve => setTimeout(resolve, waitTime));
-		readSeedMovies(p)
-	}
+    for(var p=1; p<=maxCalls; p++){
+        var waitTime = 0;
+        if(p % 40=== 0) {
+            waitTime = 10000;
+        }
+        
+        await new Promise(resolve => setTimeout(resolve, waitTime));
+        readSeedMovies(p)
+    }
 })
 
 function readSeedMovies(p){
-	fetch(baseURL+p)
-	.then(response => {
-		return response.json()
-	})
-	.then(data => {
-		var resultsLen = data.results.length;
-		for(i=0; i<resultsLen; i++){
-			let result = data.results[i];
-			let movieInfo = {
-				id: result.id,
-				vote_average: result.vote_average,
-				title: result.title,
-				popularity: result.popularity,
-				posterImage: result.poster_path,
-				original_language: result.original_language,
-				backdropImage: result.backdrop_path,
-				adult: result.adult,
-				overview: result.overview,
+    fetch(baseURL+p)
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        var resultsLen = data.results.length;
+        for(i=0; i<resultsLen; i++){
+            let result = data.results[i];
+            let movieInfo = {
+                id: result.id,
+                vote_average: result.vote_average,
+                title: result.title,
+                popularity: result.popularity,
+                posterImage: result.poster_path,
+                original_language: result.original_language,
+                backdropImage: result.backdrop_path,
+                adult: result.adult,
+                overview: result.overview,
                 release_date: result.release_date,
                 videoExists: false,
                 genre_ids: result.genre_ids
-			};
-			Movie.create(movieInfo)
-		}
-	}).catch(err => {
-		console.log(err);
-	})
-	.then(() => {
-		console.log("Data from page " + p + " was added to collection movies.")
-	});
+            };
+            Movie.create(movieInfo)
+        }
+    }).catch(err => {
+        console.log(err);
+    })
+    .then(() => {
+        console.log("Data from page " + p + " was added to collection movies.")
+    });
 }
