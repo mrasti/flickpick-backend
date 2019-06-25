b0f4d012702226e4aee5d2f984cd8a491c55ec64
Shane Vann-Shirley <ashanev@gmail.com>
2019-06-25 16:55:22 -0400

    reconfigured passport settings, experimentation using authentication on fake route

--- routes/MovieRouter.js
+++ routes/MovieRouter.js
@@ -1,10 +1,15 @@
-
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/MovieController");
+const passport = require("passport");
 
-router.get('/', controller.getRandomFive);
-router.get('/:id', controller.getById);
-router.get('/title/:title', controller.getByTitle);
+router.get("/", controller.getRandomFive);
+router.get("/:id", controller.getById);
+router.get("/title/:title", controller.getByTitle);
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  controller.deleteMovie
+);
 
-module.exports = router
+module.exports = router;
