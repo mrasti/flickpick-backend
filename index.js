5f2f8d81e6780d631a4afb2b975ecc05cc892a96
Melika <melika.r@gmail.com>
2019-06-26 14:01:52 -0400

    update index.js and package.json for deploying on heroku

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ app.use(passport.initialize());
 
 app.use("/api/movies/", MovieRouter);
 
-// app.set("port", process.env.PORT || 8080);
+app.set("port", process.env.PORT || 8080);
 
-// app.listen(app.get("port"), () => {
-//   console.log(`✅ PORT: ${app.get("port")} 🌟`);
-// });
-
-app.listen(3000, () => {
-  console.log("app listening on port 3000");
+app.listen(app.get("port"), () => {
+  console.log(`✅ PORT: ${app.get("port")} 🌟`);
 });
+
+// app.listen(3000, () => {
+//   console.log("app listening on port 3000");
+// });
