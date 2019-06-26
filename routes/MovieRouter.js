0827aff68c67c8787fc88f92a2d12f13071d30af
Shane Vann-Shirley <ashanev@gmail.com>
2019-06-26 08:24:41 -0400

    removed auth testing from movie router, added authenticate to user actions

--- routes/MovieRouter.js
+++ routes/MovieRouter.js
@@ -1,15 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/MovieController");
-const passport = require("passport");
 
 router.get("/", controller.getRandomFive);
 router.get("/:id", controller.getById);
 router.get("/title/:title", controller.getByTitle);
-router.delete(
-  "/:id",
-  passport.authenticate("jwt", { session: false }),
-  controller.deleteMovie
-);
 
 module.exports = router;
--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/UserController");
+const passport = require("passport");
 
 router.post("/signup", userController.signUp);
 router.post("/login", userController.logIn);
-router.put("/add/:userId/:movieId", userController.addMovie);
-router.put("/remove/:userId/:movieId", userController.removeMovie);
+router.put(
+  "/add/:userId/:movieId",
+  passport.authenticate("jwt", { session: false }),
+  userController.addMovie
+);
+router.put(
+  "/remove/:userId/:movieId",
+  passport.authenticate("jwt", { session: false }),
+  userController.removeMovie
+);
 
 module.exports = router;
